0bf779f72baa096baba11adf1a418f2e83ccee22
Esdras Ramos <ramosesdras94@gmail.com>
2020-09-22 13:03:04 +0200

    Add user and message resources and link them

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Mathew", password: "password")
+User.create(username: "Anna", password: "password")
+User.create(username: "Pius", password: "password")
+User.create(username: "Pedro", password: "password")
+User.create(username: "John", password: "password")
